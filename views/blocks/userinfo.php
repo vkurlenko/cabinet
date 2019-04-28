07f32c006e62324b59102d50107d5a3c0b21d11f
vkurlenko <vkurlenko@yandex.ru>
2019-04-28 18:38:12 +0300

    add block userinfo

---
+++ views/blocks/userinfo.php
@@ -0,0 +1,19 @@
+<?php
+use yii\helpers\Html;
+
+if(!$user){
+    return false;
+}
+//debug($user);
+?>
+<h1>Мои данные</h1>
+<table class="table table-striped table-bordered">
+    <tr>
+        <td><?=$user['username']?></td>
+        <td><?=$user['phone']?></td>
+        <td><?=$user['email']?></td>
+        <td><?=Html::a('Сменить пароль', '/user/update?id='.$user['id'])?></td>
+    </tr>
+</table>
+
+
